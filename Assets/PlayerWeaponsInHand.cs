be8dd0373aaeb2b9d5664331a5ef576b27f725db
IvanM71 <ivan.mrgun@gmail.com>
2023-01-30 13:12:31 +0200

    HandWeapons hide-show logic

--- Assets/PlayerWeaponsInHand.cs
+++ Assets/PlayerWeaponsInHand.cs
@@ -1,11 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
 
 namespace Apollo11
 {
-    public class PlayerWeaponsInHand : MonoBehaviour
+    public class PlayerWeaponsInHand : SerializedMonoBehaviour
     {
-        [SerializeField] private Dictionary<Enums.HandWeapon, GameObject> handWeapons;
+        [OdinSerialize] private Dictionary<Enums.HandWeapon, SpriteRenderer> handWeapons;
+
+        private void Awake()
+        {
+            DeselectWeapon();
+        }
+
+        public void SelectWeapon(Enums.HandWeapon w)
+        {
+            if (handWeapons.ContainsKey(w) == false)
+            {
+                Debug.LogError($"No weapon {w} found!");
+                return;
+            }
+            handWeapons[w].enabled = true;
+        }
+
+        public void DeselectWeapon()
+        {
+            foreach (var w in handWeapons)
+            {
+                w.Value.enabled = false;
+            }
+        }
     }
 }

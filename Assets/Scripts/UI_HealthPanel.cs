8a1d54b42bfb6150473bff610c0d65cb41b5c80a
IvanM71 <ivan.mrgun@gmail.com>
2023-02-01 18:32:50 +0200

    UI hearts

--- Assets/Scripts/UI_HealthPanel.cs
+++ Assets/Scripts/UI_HealthPanel.cs
@@ -1,23 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 namespace Apollo11
 {
     public class UI_HealthPanel : MonoBehaviour
     {
         [SerializeField] private List<Image> hearts;
-        [SerializeField] private Sprite hullHeart;
+        [SerializeField] private Sprite fullHeart;
         [SerializeField] private Sprite halfHeart;
         [SerializeField] private Sprite emptyHeart;
+
+
+        /*[Range(0, 10)]
+        [SerializeField] private int val;
+        private void FixedUpdate() => SetHealth(val);*/
+
 
         public void SetHealth(int health10)
         {
             int full = health10 / 2;
             int half = health10 % 2;
             
-            
+            for (int i = 0; i < 5; i++)
+                hearts[i].sprite = emptyHeart;
+
+            for (int i = 0; i < full; i++)
+                hearts[i].sprite = fullHeart;
+
+            if (half!=0)
+            {
+                hearts[full].sprite = halfHeart;
+            }
+
         }
     }
 }

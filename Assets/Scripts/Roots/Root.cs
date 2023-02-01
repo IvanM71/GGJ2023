51148aa194bab018f1396e0cee55d69dd7e2e043
Volodymyr <bekker.volodymyr.yu@gmail.com>
2023-02-01 16:36:12 +0200

    Fix root system

--- Assets/Scripts/Roots/Root.cs
+++ Assets/Scripts/Roots/Root.cs
@@ -3,11 +3,9 @@ namespace Apollo11.Roots
     public class Root
     {
         public Enums.RootStages stage;
-        public bool[] _growDirections; // 0 - up, 1 - down, 2 - left, 3 - right
         public Root(Enums.RootStages stage)
         {
             this.stage = stage;
-            _growDirections = new bool[4] { false, false, false, false };
         }
     }
 }

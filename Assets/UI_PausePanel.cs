330069ae1e412e0eedf3f8d3c69b11bb2f773463
IvanM71 <ivan.mrgun@gmail.com>
2023-02-04 19:10:05 +0200

    Pause window

--- Assets/UI_PausePanel.cs
+++ Assets/UI_PausePanel.cs
@@ -9,11 +9,27 @@ namespace Apollo11
 
         private bool _inPause;
 
+        private void Awake()
+        {
+            pausePanel.SetActive(false);
+        }
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                
+                if (_inPause)
+                {
+                    Time.timeScale = 1f;
+                    pausePanel.SetActive(false);
+                    _inPause = false;
+                }
+                else
+                {
+                    Time.timeScale = 0f;
+                    pausePanel.SetActive(true);
+                    _inPause = true;
+                }
             }
         }
     }

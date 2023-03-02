957eae4a49167e845b29edcd76c5f84531da8ed1
IvanM71 <ivan.mrgun@gmail.com>
2023-03-02 19:29:03 +0200

    bug fix 2

--- Assets/Scripts/UI/SceneSelector.cs
+++ Assets/Scripts/UI/SceneSelector.cs
@@ -13,11 +13,13 @@ namespace Apollo11.UI
 
         public void OpenLevel(int level)
         {
-            if (level < 1 || level >= SceneManager.sceneCount)
+            if (level < 1 || level >= SceneManager.sceneCountInBuildSettings)
             {
-                Debug.LogError("Tried to load non existing level!");
+                Debug.LogError($"Tried to load non existing level ({level})!");
                 OpenMainMenu();
+                return;
             }
+            LevelManager.Instance.currentLevel = level;
             SceneManager.LoadScene(level);
         }
     }

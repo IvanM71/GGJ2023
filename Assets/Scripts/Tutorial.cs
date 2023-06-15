dcf32def6054ba683df28f1a4acf73d2ae87537a
bekker-volodymyr <bekker.volodymyr.yu@gmail.com>
2023-06-15 15:29:44 +0300

    Level 1 tutorial v0

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,60 @@
+using Apollo11.Core;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Apollo11
+{
+    public class Tutorial : MonoBehaviour
+    {
+        [SerializeField] private GameObject tutorialPanel;
+        [SerializeField] private int pageCount;
+        [SerializeField] private List<Sprite> firstScreenList;
+        [SerializeField] private List<Sprite> secondScreenList;
+        [SerializeField] private List<string> pageText;
+
+        [SerializeField] private Image firstScreenshot;
+        [SerializeField] private Image secondScreenshot;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Button nextButton;
+
+        [SerializeField] private Sprite startButtonSprite;
+
+        private int currentPage = 0;
+
+        private void Awake()
+        {
+            SystemsLocator.Inst.InPause = true;
+            firstScreenshot.sprite = firstScreenList[currentPage];
+            firstScreenshot.SetNativeSize();
+            secondScreenshot.sprite = secondScreenList[currentPage];
+            secondScreenshot.SetNativeSize();
+            text.text = pageText[currentPage];
+        }
+        public void NextPageClick()
+        {
+            
+            if(currentPage == pageCount)
+            {
+                tutorialPanel.SetActive(false);
+                SystemsLocator.Inst.InPause = false;
+            }
+            else
+            {
+                currentPage++;
+                firstScreenshot.sprite = firstScreenList[currentPage];
+                firstScreenshot.SetNativeSize();
+                secondScreenshot.sprite = secondScreenList[currentPage];
+                secondScreenshot.SetNativeSize();
+                text.text = pageText[currentPage];
+                if (currentPage == pageCount - 1)
+                {
+                    nextButton.image.sprite = startButtonSprite;
+                    currentPage++;
+                }
+            }
+        }
+    }
+}
